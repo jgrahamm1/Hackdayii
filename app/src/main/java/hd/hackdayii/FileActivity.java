c6706d20e95dd649fa04e535311ce5672e52ebe6
jgrahamm1 <jack.m.graham@gmail.com>
2017-04-08 13:15:44 -0400

    added action button

--- app/src/main/java/hd/hackdayii/FileActivity.java
+++ app/src/main/java/hd/hackdayii/FileActivity.java
@@ -22,6 +22,8 @@ import android.widget.Toast;
 
 public class FileActivity extends ListActivity {
 
+    private static final int READ_REQUEST_CODE = 42;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +41,11 @@ public class FileActivity extends ListActivity {
 
             @Override
             public void onClick(View view) {
-            Toast.makeText(getApplicationContext(), "Action Button Pressed!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Action Button Pressed!", Toast.LENGTH_SHORT).show();
+//                performFileSearch();
+//                Intent docIntent = new Intent(Intent.ACTION_GET_CONTENT);
+//                //mediaIntent.setType("video/*"); //set mime type as per requirement
+//                startActivityForResult(docIntent, 2);
             }
         });
     }
@@ -48,6 +54,28 @@ public class FileActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         String item = (String) getListAdapter().getItem(position);
         Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    public void performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+//        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+//
+//        // Filter to only show results that can be "opened", such as a
+//        // file (as opposed to a list of contacts or timezones)
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//
+//        // Filter to show only images, using the image MIME data type.
+//        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+//        // To search for all documents available via installed storage providers,
+//        // it would be "*/*".
+//        intent.setType("image/*");
+//
+//        startActivityForResult(intent, READ_REQUEST_CODE);
     }
 
 }
