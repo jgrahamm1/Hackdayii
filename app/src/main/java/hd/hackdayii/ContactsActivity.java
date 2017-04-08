1d6e900e9aa61773aa4c744a7207c659ccb568ff
jgrahamm1 <jack.m.graham@gmail.com>
2017-04-08 10:50:00 -0400

    basic framework -v0.0.2

--- app/src/main/java/hd/hackdayii/ContactsActivity.java
+++ app/src/main/java/hd/hackdayii/ContactsActivity.java
@@ -3,7 +3,10 @@ package hd.hackdayii;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by jgraham on 4/8/17.
@@ -12,11 +15,17 @@ import android.widget.ArrayAdapter;
 public class ContactsActivity extends ListActivity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile is dead",
+                "Blackberry seriously sucks", "WebOS", "Ubuntu Sucks", "Windows7 Sucks", "Max OS X Is Best",
+                "Linux is ok", "OS/2" };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/hd/hackdayii/LstAdapter.java
@@ -0,0 +1,8 @@
+package hd.hackdayii;
+
+/**
+ * Created by jgraham on 4/8/17.
+ */
+
+public class LstAdapter {
+}
